dcb09218f41090611bd0c6791b70572003e363ab
GNooNJo <jjjchoon@nate.com>
2024-02-17 20:15:11 +0900

    feat: dropdown function

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import { DropdownProvider, useDropdown } from "./DropdownContext";
 
 const DropdownButton = () => {
-  return <button>Dropdown Button</button>;
+  const { selectedItem, toggleDropdown } = useDropdown();
+
+  return (
+    <button onClick={toggleDropdown}>{selectedItem ? selectedItem : "Dropdown Button"}</button>
+  );
 };
 
 const DropdownMenu = ({ items }) => {
+  const { isOpen, toggleDropdown, selectItem } = useDropdown();
+
+  if (!isOpen) return null;
+
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li
+          key={item}
+          onClick={() => {
+            // 아이템 클릭 시
+            selectItem(item); // (1) selected item : 클릭한 item
+            toggleDropdown(); // (2) Dropdown 닫힘
+          }}
+        >
+          {item}
+        </li>
       ))}
     </ul>
   );
@@ -17,6 +34,8 @@ const DropdownMenu = ({ items }) => {
 
 const Dropdown = ({ items }) => {
   return (
+    // REVIEW: DropdownProvider로 감싸는 이유?
+    // DropdownContext를 import했기 때문에 사용하지 않아도 되지 않나?
     <DropdownProvider>
       <div>
         <DropdownButton />
